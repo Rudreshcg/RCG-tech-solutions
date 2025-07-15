9fb89f53579ad8abf0e17112225565c6de8f46f9
Rudreshcg <rudreshcsbg82@gmail.com>
2025-07-15 12:02:03 +0530

    Updated code

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,7 +1,13 @@
+// src/app/blogs/[slug]/page.tsx
 import BlogPostDetailClient from "../../components/blogs/BlogPostDetailClient";
 
-export default function Page({ params }: { params: { slug: string } }) {
-  return <BlogPostDetailClient slug={params.slug} />;
+// Define the PageProps interface with the correct type for params
+interface PageProps {
+  params: {
+    slug: string;
+  };
 }
 
-
+export default function Page({ params }: PageProps) {
+  return <BlogPostDetailClient slug={params.slug} />;
+}
